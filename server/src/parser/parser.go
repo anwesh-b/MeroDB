ec3ffa17e87f0e3d6e66a97c34f2abc069aa3b80
Anwesh <abperni@gmail.com>
2023-07-08 21:02:18 +0545

    Add dynamic tablename in queries

--- server/src/parser/parser.go
+++ server/src/parser/parser.go
@@ -1,20 +1,78 @@
 package parser
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"os"
 	"strconv"
 	"strings"
+	"text/tabwriter"
 
 	cli "github.com/anwesh-b/MeroDB/lib/cli"
 )
 
-const fileName = "local.db"
+const filePath = "../data/"
+const referenceFilePath = filePath + "tables.db"
+
+func doesTableExist(tableName string) (bool, error) {
+	data, err := ioutil.ReadFile(referenceFilePath)
+	if err != nil {
+		fmt.Println(err)
+		return false, errors.New("error in reading db. db file might be corrupt")
+	}
+
+	lines := strings.Split(string(data), "\n")
+
+	for _, line := range lines {
+		if strings.HasPrefix(line, tableName+" ") {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
+func getReferenceTableDetails(tableName string) ([]string, error) {
+	data, err := ioutil.ReadFile(referenceFilePath)
+
+	if err != nil {
+		fmt.Println(err)
+		return nil, errors.New("error in reading db. db file might be corrupt")
+	}
+
+	lines := strings.Split(string(data), "\n")
+
+	for _, line := range lines {
+		if strings.HasPrefix(line, tableName+" ") {
+			columns := strings.Split(line, " ")
+
+			return columns[1:], nil
+		}
+	}
+
+	return nil, errors.New("table does not exist")
+}
 
 func insert(str string) {
 	s := strings.Split(str, " ")
 
-	datas := s[1:]
+	tableName := s[1]
+
+	columns, err := getReferenceTableDetails(tableName)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	datas := s[2:]
+
+	if len(datas) != len(columns) {
+		fmt.Println("Column count does not match. The columns are: " + strings.Join(columns, ", "))
+		return
+	}
 
 	id, err := strconv.Atoi(datas[0])
 	if err != nil {
@@ -22,7 +80,10 @@ func insert(str string) {
 		return
 	}
 
+	fileName := filePath + tableName + ".db"
+
 	file, err := ioutil.ReadFile(fileName)
+
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -31,23 +92,38 @@ func insert(str string) {
 	lines := strings.Split(string(file), "\n")
 
 	for _, line := range lines {
-		if strings.HasPrefix(line, strconv.Itoa(id)) {
+		if strings.HasPrefix(line, strconv.Itoa(id)+" ") {
 			cli.CLog("Data with id " + strconv.Itoa(id) + " already exists")
 			return
 		}
 	}
 
-	output := string(file) + strconv.Itoa(id) + "\t" + datas[1] + "\t" + datas[2] + "\n"
-	err = ioutil.WriteFile(fileName, []byte(output), 0644)
+	output := strings.Join(datas, " ") + "\n"
+
+	f, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Println(err)
+	}
+	defer f.Close()
+	if _, err := f.WriteString(output); err != nil {
+		log.Println(err)
 	}
 
 	cli.CLog("Insert success")
 }
 
 func selectData(str string) {
+	tableName := strings.Split(str, " ")[1]
+
+	columns, err := getReferenceTableDetails(tableName)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fileName := filePath + tableName + ".db"
+
+	fmt.Println("debug")
 	data, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		fmt.Println(err)
@@ -55,26 +131,46 @@ func selectData(str string) {
 	}
 
 	lines := strings.Split(string(data), "\n")
-	for _, line := range lines {
-		items := strings.Split(line, "\t")
 
-		for _, item := range items {
-			fmt.Print(item + "\t")
-		}
-		fmt.Println("")
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', tabwriter.AlignRight|tabwriter.Debug)
+
+	fmt.Fprintln(w, strings.Join(columns, "\t"))
+
+	for _, line := range lines {
+		items := strings.Split(line, " ")
+
+		fmt.Fprintln(w, strings.Join(items, "\t"))
 	}
+
+	w.Flush()
 }
 
 func update(str string) {
 	s := strings.Split(str, " ")
 
-	datas := s[1:]
+	tableName := s[1]
+
+	columns, err := getReferenceTableDetails(tableName)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	datas := s[2:]
+
+	if len(datas) != len(columns) {
+		fmt.Println("Column count does not match. The columns are: " + strings.Join(columns, ", "))
+		return
+	}
 
 	id, err := strconv.Atoi(datas[0])
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+
+	fileName := filePath + tableName + ".db"
 
 	data, err := ioutil.ReadFile(fileName)
 
@@ -88,10 +184,10 @@ func update(str string) {
 	var didUpdate bool = false
 
 	for i, line := range lines {
-		if strings.HasPrefix(line, strconv.Itoa(id)) {
-			lines[i] = strconv.Itoa(id) + "\t" + datas[1] + "\t" + datas[2]
+		if strings.HasPrefix(line, strconv.Itoa(id)+" ") {
+			lines[i] = strings.Join(datas, " ")
 			output := strings.Join(lines, "\n")
-			err = ioutil.WriteFile(fileName, []byte(output), 0644)
+			err = ioutil.WriteFile(fileName, []byte(output), 0644) // can optimize this code
 			if err != nil {
 				fmt.Println(err)
 				return
@@ -109,14 +205,24 @@ func update(str string) {
 
 func deleteData(str string) {
 	s := strings.Split(str, " ")
+	tableName := s[1]
 
-	datas := s[1:]
+	_, err := getReferenceTableDetails(tableName)
 
-	id, err := strconv.Atoi(datas[0])
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+
+	deleteId := s[2]
+
+	id, err := strconv.Atoi(deleteId)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fileName := filePath + tableName + ".db"
 
 	data, err := ioutil.ReadFile(fileName)
 
@@ -130,7 +236,7 @@ func deleteData(str string) {
 	var didDelete bool = false
 
 	for i, line := range lines {
-		if strings.HasPrefix(line, strconv.Itoa(id)) {
+		if strings.HasPrefix(line, strconv.Itoa(id)+" ") {
 			lines[i] = ""
 
 			copy(lines[i:], lines[i+1:]) // Shift one index left, until the i.
@@ -151,18 +257,103 @@ func deleteData(str string) {
 		return
 	}
 
-	cli.CLog("Couldnot find the data, delete failed")
+	cli.CLog("Couldnot find the data, delete did not occur")
+}
+
+func createTable(str string) {
+	s := strings.Split(str, " ")
+
+	tableName := s[1]
+	tableColumns := append([]string{tableName, "id"}, s[2:]...)
+
+	isTableExisting, err := doesTableExist(tableName)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if isTableExisting {
+		fmt.Println("Table already exists")
+		return
+	}
+
+	fileName := filePath + tableName + ".db"
+
+	f, err := os.Create(fileName)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	f, err = os.OpenFile(referenceFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Println(err)
+	}
+	defer f.Close()
+	if _, err := f.WriteString(strings.Join(tableColumns, " ") + "\n"); err != nil {
+		log.Println(err)
+	}
+}
+
+func dropTable(str string) {
+	s := strings.Split(str, " ")
+
+	tableName := s[1]
+
+	data, err := ioutil.ReadFile(referenceFilePath)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	lines := strings.Split(string(data), "\n")
+
+	fileName := filePath + tableName + ".db"
+
+	for i, line := range lines {
+		if strings.HasPrefix(line, tableName+" ") {
+			lines[i] = ""
+
+			copy(lines[i:], lines[i+1:]) // Shift one index left, until the i.
+			lines = lines[:len(lines)-1] // Remove last element (write zero value).
+
+			output := strings.Join(lines, "\n")
+			err = ioutil.WriteFile(referenceFilePath, []byte(output), 0644)
+
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			e := os.Remove(fileName)
+			if e != nil {
+				log.Fatal(e)
+			}
+
+			return
+		}
+	}
+
+	fmt.Println("Table not found and not deleted")
 }
 
 func EvaluateInput(str string) {
-	if strings.HasPrefix(str, "insert") {
+	if strings.HasPrefix(str, "insert ") {
 		insert(str)
-	} else if strings.HasPrefix(str, "select") {
+	} else if strings.HasPrefix(str, "select ") {
 		selectData(str)
-	} else if strings.HasPrefix(str, "update") {
+	} else if strings.HasPrefix(str, "update ") {
 		update(str)
-	} else if strings.HasPrefix(str, "delete") {
+	} else if strings.HasPrefix(str, "delete ") {
 		deleteData(str)
+	} else if strings.HasPrefix(str, "create ") {
+		createTable(str)
+	} else if strings.HasPrefix(str, "drop ") {
+		dropTable(str)
 	} else {
 		cli.CLog("Invalid command")
 	}
